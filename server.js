ad6559bd9d965701ea5b419d8a8ee13233d32f8b
Bedo <bedonassef71@gmail.com>
2023-10-24 14:55:36 +0300

    error handling

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const {dbConnection} = require("./connect/mongo");
-const config = require('./config/index.config')
-
-
+const config = require('./config/index.config');
+const globalExceptionFilter = require('./common/filters/global-exception.filter');
 const app = express();
 
+process.on('uncaughtException', err => {
+    console.log(`Uncaught Exception:`)
+    console.log(err, err.stack);
+
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled rejection at ', promise, `reason:`, reason);
+    process.exit(1)
+})
+
+app.use(globalExceptionFilter)
 dbConnection
     .then(
         console.log('connected to database successfully'),
